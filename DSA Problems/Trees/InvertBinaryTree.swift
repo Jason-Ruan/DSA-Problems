d11ecfb2228f4666297887c7e2b4115a7259111f
Jason Ruan <jasonruan@pursuit.org>
2020-04-14 16:04:06 -0400

    Adds InvertBinaryTree solution

--- DSA Problems/Trees/InvertBinaryTree.swift
+++ DSA Problems/Trees/InvertBinaryTree.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+//Question 2
+
+//Invert a binary tree.
+
+//Example:
+//Input:
+
+/*
+ 4
+ /   \
+ 2     7
+ / \   / \
+ 1   3 6   9
+ 
+ Output:
+ 
+ 4
+ /   \
+ 7     2
+ / \   / \
+ 9   6 3   1
+ 
+ */
+
+class InverseBinaryTreeSolution {
+    
+    private var queue: [TreeNode?] = []
+    
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let root = root else { return nil }
+        
+        queue.append(root)
+        
+        while !queue.isEmpty {
+            invertNode(node: queue.removeFirst())
+        }
+        
+        return root
+        
+    }
+    
+    func invertNode(node: TreeNode?) {
+        guard let node = node else { return }
+        
+        let right = node.right
+        node.right = node.left
+        node.left = right
+        
+        queue.append(node.left)
+        queue.append(node.right)
+        
+    }
+    
+}
